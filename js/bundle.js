5709d46aaf1ab77ead54e6e6b8e6fdd48876c217
baripembo <erikawei@gmail.com>
2022-03-09 10:32:26 -0500

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1749,7 +1749,7 @@ function transitionBarChart(data){
 
 function vizTrack(view, content) {
   mpTrack(view, content);
-  gaTrack('viz interaction', 'switch viz', 'oad covid-19 / '+view, content);
+  gaTrack('viz interaction', 'switch viz', 'ukr data explorer / '+view, content);
 }
 
 function mpTrack(view, content) {
