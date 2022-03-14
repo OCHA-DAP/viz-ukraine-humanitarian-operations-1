ea2b298482e2adbfc09329cd816d20426fa789b2
baripembo <erikawei@gmail.com>
2022-03-14 17:31:09 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -4316,7 +4316,8 @@ function initCountryPanel() {
   //refugees
   var refugeesDiv = $('.country-panel .refugees .panel-inner');
   createFigure(refugeesDiv, {className: 'refugees', title: 'Refugee arrivals from Ukraine (total)', stat: shortenNumFormat(regionalData['#affected+refugees']), indicator: '#affected+refugees'});
-  createFigure(refugeesDiv, {className: 'pin', title: 'People in Need (estimated)', stat: shortenNumFormat(data['#inneed+ind']), indicator: '#inneed+ind'});
+  //createFigure(refugeesDiv, {className: 'pin', title: 'People in Need (estimated)', stat: shortenNumFormat(data['#inneed+ind']), indicator: '#inneed+ind'});
+  createFigure(refugeesDiv, {className: 'idps', title: 'Internally Displaced People (estimated)', stat: shortenNumFormat(data['#affected+idps']), indicator: '#affected+idps'});
   createFigure(refugeesDiv, {className: 'casualties-killed', title: 'Civilian Casualties - Killed', stat: numFormat(data['#affected+killed']), indicator: '#affected+killed'});
   createFigure(refugeesDiv, {className: 'casualties-injured', title: 'Civilian Casualties - Injured', stat: numFormat(data['#affected+injured']), indicator: '#affected+injured'});
 
@@ -4475,8 +4476,6 @@ $( document ).ready(function() {
       refugeeTimeseriesData = allData.refugees_series_data;
       acledData = allData.fatalities_data;
       sourcesData = allData.sources_data;
-
-      console.log(acledData)
 
       borderCrossingData = data[1];
       regionBoundaryData = data[2].features;
